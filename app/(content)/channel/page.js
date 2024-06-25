68c7750ffd8b7bb746ea54b6594a67dbfb1200c4
winternotseason <winternotseason@naver.com>
2024-06-25 12:41:07 +0900

    css

--- app/(content)/channel/page.js
+++ app/(content)/channel/page.js
@@ -1,14 +1,6 @@
 import YouTubePlayer from "@/components/youtube";
+import Image from "next/image";
 import classes from "./page.module.css";
 export default async function PageChannelV() {
-  return (
-    <>
-      <video className={classes.video} autoPlay loop muted>
-        <source
-          src="https://videos.pexels.com/video-files/856857/856857-uhd_2732_1440_30fps.mp4"
-          type="video/mp4"
-        />
-      </video>
-    </>
-  );
+  return <div className={classes.background} />;
 }
--- components/youtube.js
+++ components/youtube.js
@@ -4,12 +4,8 @@ import YouTube from "react-youtube";
 
 export default function YouTubePlayer({ videoId }) {
   const opts = {
-    height: "900",
+    height: "700",
     width: "100%",
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
-    },
   };
 
   return <YouTube videoId={videoId} opts={opts} />;
